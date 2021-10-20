9338fff0ece552e8dfdb2b6bfc80f7ae672988e5
FrancoBenjamínFormigo <francobf15@gmail.com>
2021-10-20 17:06:18 -0300

    estructura base express

---
+++ site/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 8080;
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/index.html'))
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening to Port: ${PORT}`)
+    console.log(`http://localhost:${PORT}`)
+});
