87319fac6f36b4370fdc5e5e0f2568b480f950be
FrancoBenjamínFormigo <francobf15@gmail.com>
2021-10-23 10:37:23 -0300

    fix estructura base

--- site/app.js
+++ site/app.js
@@ -6,7 +6,7 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/index.html'))
+    res.sendFile(path.join(__dirname + '/views/home.html'))
 });
 
 
