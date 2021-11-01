1c6b7f7d7445d2fb773a76ab4233b762a17985a5
Alen-Rayen <alen-rayen@hotmail.com>
2021-11-01 13:02:30 -0300

    carrito actualizado

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 8080;
+app.use(express.static('public'));
+
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname + '/views/cart.html'))
+})
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/home.html'))
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening to Port: ${PORT}`)
+    console.log(`http://localhost:${PORT}`)
+});
